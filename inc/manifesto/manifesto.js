104609f54c0b4c91c40d20c8806da2da265b414e
Raafi Rivero <raafi.rivero@gmail.com>
2014-01-21 12:57:42 -0500

    smoke bg

--- inc/manifesto/manifesto.js
+++ inc/manifesto/manifesto.js
@@ -12,7 +12,23 @@ $(".deal").squishy({maxSize: 74});
 
 
 
+$('#bg').smartBackgroundResize({
+	image: 'http://dunk.site/img/products/smokeloop-neue.gif' // relative or absolute path to background image file				
+});
 
+
+//	$('#bg').delay(2000).fadeOut(2000);
+
+$('.manny-buy').hide().delay(2000).fadeIn(600);
+
+var paras = $('p'),  
+    i = 0;  
+
+paras.css("opacity",0).delay(3000);
+
+(function paraFade() {  
+  $(paras[i++] || []).fadeTo	('200', 1, arguments.callee);  
+})();  
 
 
 
