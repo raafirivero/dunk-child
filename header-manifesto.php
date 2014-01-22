df72ceec9f4654080533bee924b8dbb8f69464f8
Raafi Rivero <raafi.rivero@gmail.com>
2014-01-22 14:55:40 -0500

    temporarily turn off caching

--- header-manifesto.php
+++ header-manifesto.php
@@ -7,9 +7,15 @@ global $flatsome_opt;
 <!--[if lte IE 9 ]><html class="ie lt-ie9" <?php language_attributes(); ?>> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+
+<!-- turn off caching for dev purposes -->
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+<META HTTP-EQUIV="Expires" CONTENT="-1">
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
