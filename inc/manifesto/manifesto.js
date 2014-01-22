91adba9683fae7816ed3402a6b1b13c42d6edf5c
Raafi Rivero <raafi.rivero@gmail.com>
2014-01-22 15:43:58 -0500

    slower animation

--- inc/manifesto/manifesto.js
+++ inc/manifesto/manifesto.js
@@ -4,13 +4,16 @@ jQuery(document).ready(function($) {
 $(window).load(function(){
 	$(".deal").css({
                  transform: "rotateX(-180deg)",
-                 opacity: 0
+                 opacity: 0,
+                 scale: .85
             });
-	$(".deal").squishy({maxSize: 74}).delay(150).transition({
+	$(".deal").squishy({maxSize: 74}).delay(1900).transition({
 	  perspective: '100px',
 	  rotateX: '0deg',
-	  opacity: 1
-	}, 600);
+	  opacity: 1,
+	  scale: 1,
+	  y: -5
+	}, 700);
 });
 
 	$('#bg').smartBackgroundResize({
@@ -25,7 +28,7 @@ $('.manny-buy').hide().delay(2000).fadeIn(600);
 var paras = $('p'),  
     i = 0;  
 
-paras.css("opacity",0).delay(2700);
+paras.css("opacity",0).delay(5000);
 
 $(function paraFade() {
 	
@@ -34,7 +37,7 @@ $(function paraFade() {
 		$(this).delay((i++) * 300).fadeTo(800, 1); 
 	});
 	// console.log('callback');
-	$('#bg').fadeOut(3600, function(){
+	$('#bg').fadeOut(5500, function(){
 		$(this).remove();
 	});
 	
@@ -48,12 +51,12 @@ $('.topsclub').hover(function(){
     $(this).transition({ scale: 1 });
   });
 
-$('.logocurve').transition({ scale:.8, opacity: 0},0).transition({ scale: 1, opacity: 1},700);
+$('.logocurve').transition({ scale:.6, opacity: 0},0).delay(1400).transition({ scale: .9, opacity: 1},1600);
 
-$('.eyeproh2').css({"opacity":0,"scale":"50%"}).delay(700).transition({ scale: 1.1, opacity: 1 }, 700).delay(100).transition({ scale: .8 }, 300).transition({ rotate: '90deg', scale: .4, x: -20, y: 00}).fadeOut(300);
+$('.eyeproh2').transition({ scale: .8, opacity: 0 },0).delay(2500).transition({ scale: 1.1, opacity: 1 }, 1000).delay(300).transition({ scale: .8 }, 400).transition({ rotate: '90deg', scale: .3, x: 0, y:0}).fadeOut(300);
 
 
-
+// $('.topsflat').delay(7000).transition({ scale: .7, opacity: .8 },800)
 
 
 
